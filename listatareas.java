ad02170db7c8ce38214ba46a82d76ce791dc8210
Kahmzhen <143685386+Kahmzhen@users.noreply.github.com>
2023-09-06 08:55:16 -0600

    Add files via upload

---
+++ listatareas.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication26;
+
+/**
+ *
+ * @author Usuario
+ */
+public class  listatareas{
+    public static void main(String[] args) {
+   int ListadeTareas;
+    String Tarea=" LavarTrapos ";
+    String Tarea1=" LavarTang ";
+    String Tarea2=" LavarTrastes ";
+    String Tarea3=" LavarTitan ";
+    String Tarea4=" LavarCasa ";
+    
+    System.out.println("ListadeTareas"+Tarea+Tarea1+Tarea2+Tarea3+Tarea4);     
+    }
+    
+}
+     
+            
+            
+      
+    
+    
+    
+    
+   
+          
+
