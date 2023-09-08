0a4463c4bedba8de0a1e55274ebf2c114d725d7a
Kahmzhen <143685386+Kahmzhen@users.noreply.github.com>
2023-09-08 08:40:08 -0600

    Add files via upload

---
+++ AreaDeUnTriangulo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package area.de.un.triangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaDeUnTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int base  = 3;
+                int altura = 4 ;
+                        
+                        int resultado = (base * altura / 2 );
+                          System.out.println(resultado);      
+    }
+    
+}
